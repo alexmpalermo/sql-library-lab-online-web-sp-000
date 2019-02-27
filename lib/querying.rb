0d97e6648e606528713650f40745575f4440a6ec
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-27 03:48:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  GROUP BY chartacters.id 
+  GROUP BY characters.id 
   ORDER BY LENGTH(characters.motto)
   LIMIT 1
   ;"
