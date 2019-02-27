4bdbbe41b3391262115a16654322a47b041c796d
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-27 04:18:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN characters ON character_books.character_id = characters.id
   JOIN books ON character_books.book_id = books.id 
   GROUP BY characters.id 
+  ORDER BY COUNT(books.title) DESC 
   ;"
 end
